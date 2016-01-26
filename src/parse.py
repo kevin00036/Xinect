2efcbbbe76b248679a64a49ceff341e1d335443e
step5 <kevin00050@gmail.com>
2016-01-26 15:16:15 +0800

    DNN with 4 class

--- src/parse.py
+++ src/parse.py
@@ -4,6 +4,12 @@ from os import path
 import matplotlib.pyplot as plt
 from sklearn import svm, cluster, decomposition, mixture
 from hmmlearn import hmm
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.recurrent import LSTM
+from keras.layers.embeddings import Embedding
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import SGD, Adam
 import random
 
 def cos_sim(a, b):
@@ -19,7 +25,7 @@ def load_data(data_type, num, label):
     for fn in filenames:
         d = [[float(x) for x in l.strip().split()] for l in open(fn)]
         d = np.array(d)
-        d = np.concatenate([d[:, 46:46],d[:, 49:110]], axis=1)
+        d = np.concatenate([d[:, 1:46],d[:, 49:110]], axis=1)
         data.append(d)
 
     lab = [np.array([label] * x.shape[0]) for x in data]
@@ -43,18 +49,85 @@ def compress(l, thr=2):
     return ret
 
 d1, _ = load_data('random', 1, 1)
-d_l, _ = load_data('lock', 10, 1)
-d_s, _ = load_data('scuba', 7, 1)
-d_sk, _ = load_data('skitter', 1, 1)
-d_p, _ = load_data('punch', 1, 1)
+d_em, _ = load_data('stand', 2, 1)
+d_l, _ = load_data('lock', 15, 1)
+d_s, _ = load_data('scuba', 12, 1)
+d_sk, _ = load_data('skitter', 5, 1)
+d_p, _ = load_data('punch', 5, 1)
 d_m, _ = load_data('mix', 1, 1)
+d_t, _ = load_data('test', 1, 1)
 
 d_train = np.concatenate(d1, axis=0)
+d_em = np.concatenate(d_l, axis=0)
 d_l = np.concatenate(d_l, axis=0)
 d_s = np.concatenate(d_s, axis=0)
 d_sk = np.concatenate(d_sk, axis=0)
 d_p = np.concatenate(d_p, axis=0)
 d_m = np.concatenate(d_m, axis=0)
+d_t = np.concatenate(d_t, axis=0)
+
+model = Sequential()
+# model.add(BatchNormalization(input_shape=(d_l.shape[1],)))
+model.add(Dense(output_dim=128, input_dim=d_l.shape[1], init="glorot_uniform"))
+model.add(Activation("sigmoid"))
+model.add(Dense(output_dim=64, init="glorot_uniform"))
+model.add(Activation("sigmoid"))
+# model.add(Dense(output_dim=64, init="glorot_uniform"))
+# model.add(Activation("sigmoid"))
+# model.add(Embedding(d_train.shape[1], 128, input_length=500))
+# model.add(LSTM(output_dim=128, activation='sigmoid', inner_activation='hard_sigmoid'))
+model.add(Dense(output_dim=4, init="glorot_uniform"))
+model.add(Activation("softmax"))
+EPOCH = 50
+
+# model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.001, momentum=0.9, nesterov=True))
+model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.001))
+
+X = []
+Y = []
+W = {}
+for i, z in enumerate([d_l, d_s, d_sk, d_p]):
+    X.append(z)
+    oz = np.zeros([4])
+    oz[i] = 1
+    Y.extend([oz]*z.shape[0])
+    # W.extend([1./z.shape[0]]*z.shape[0])
+    W[i] = 1./z.shape[0]
+
+X = np.concatenate(X, axis=0)
+Y = np.array(Y)
+# W = np.array(W)
+rat = len(Y) / sum(W)
+for i in W:
+    W[i] *= rat
+
+perm = list(range(len(Y)))
+random.shuffle(perm)
+X = X[perm,:]
+Y = Y[perm]
+# W = W[perm]
+
+xmean = np.mean(X, axis=0)
+xstd = np.std(X, axis=0)
+
+BATCH = 64
+model.fit(X, Y, nb_epoch=EPOCH, batch_size=BATCH, class_weight=W)
+
+dd = np.concatenate([d_l, d_s, d_m], axis=0)
+proba = model.predict_proba(d_t, batch_size=BATCH)
+
+K = 25
+pf = [np.mean(proba[x:x+K, :], axis=0) for x in range(proba.shape[0]-K)]
+pf = np.array(pf)
+
+plt.plot(pf[:, 0], label='lock')
+plt.plot(pf[:, 1], label='scuba')
+plt.plot(pf[:, 2], label='skitter')
+plt.plot(pf[:, 3], label='punch')
+plt.legend()
+plt.show()
+
+exit()
 
 N_cluster = 20
 cluster_model = mixture.GMM(n_components=N_cluster, random_state=514)
@@ -77,13 +150,13 @@ N_State = N_cluster
 
 hmm_l = hmm.GaussianHMM(n_components=N_State, covariance_type="diag", n_iter=1000, algorithm="map")
 hmm_l.fit([d_l])
-hmm_s = hmm.GaussianHMM(n_components=N_State, covariance_type="diag", n_iter=1000, algorithm="map", init_params="mc")
-hmm_s.means_ = cluster_model.means_
-hmm_s.covars_ = cluster_model.covars_
+hmm_s = hmm.GaussianHMM(n_components=N_State, covariance_type="diag", n_iter=1000, algorithm="map")
+# hmm_s.means_ = cluster_model.means_
+# hmm_s.covars_ = cluster_model.covars_
 hmm_s.fit([d_s])
 # tmp = hmm_s.transmat_ + 0.02
 # hmm_s.transmat_ = tmp / np.sum(tmp, axis=1)
-print(hmm_s.transmat_)
+# print(hmm_s.transmat_)
 hmm_sk = hmm.GaussianHMM(n_components=N_State, covariance_type="diag", n_iter=1000, algorithm="map")
 hmm_sk.fit([d_sk])
 hmm_p = hmm.GaussianHMM(n_components=N_State, covariance_type="diag", n_iter=1000, algorithm="map")
@@ -113,8 +186,8 @@ step = 5
 window = 25
 for i in range(window, dd.shape[0]-window, step):
     sc_l.append(hmm_l.score(dd[i-window:i+window, :]))
-    # sc_s.append(hmm_s.score(dd[i-window:i+window, :]))
-    sc_s.append(np.mean(gg[i-window:i+window]))
+    sc_s.append(hmm_s.score(dd[i-window:i+window, :]))
+    # sc_s.append(np.mean(gg[i-window:i+window]))
     # sc_sk.append(hmm_sk.score(dd[i-window:i+window, :]))
     # sc_p.append(hmm_p.score(dd[i-window:i+window, :]))
 
@@ -129,7 +202,7 @@ sc_p /= -np.mean(sc_p)
 
 plt.plot(sc_l, label='lock')
 plt.plot(sc_s, label='scuba')
-plt.plot(sc_sk, label='skitter')
-plt.plot(sc_p, label='punch')
+# plt.plot(sc_sk, label='skitter')
+# plt.plot(sc_p, label='punch')
 plt.legend()
 plt.show()
